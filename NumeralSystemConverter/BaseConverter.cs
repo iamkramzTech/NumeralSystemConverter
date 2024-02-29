bf9411e1039f02691bdf5ee49860b90e9033e7c6
iamkramzTech <bernaldezm88@yahoo.com>
2024-02-29 10:42:20 +0800

    Adding BinaryToDecimal Conversion Test

--- NumeralSystemConverter/BaseConverter.cs
+++ NumeralSystemConverter/BaseConverter.cs
@@ -1,10 +1,57 @@
-﻿namespace NumeralSystemConverter
+﻿using System.Text;
+
+namespace NumeralSystemConverter
 {
+    /// <summary>
+    /// A static class for Base Number Conversion
+    /// static class means cannot be instantiated to create an object
+    /// </summary>
     public static class BaseConverter
     {
+
+        /// <summary>
+        /// Converts a non-negative decimal number to its binary equivalent as a string.
+        /// </summary>
+        /// <param name="decimalNumber">The non-negative integer to convert. Must be a non-negative integer.</param>
+        /// <returns>A string representing the binary equivalent of the decimal number.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="decimalNumber"/> is negative.</exception>
         public static string DecimalToBinary(int decimalNumber)
         {
-            return Convert.ToString(decimalNumber, 2);
+            if (decimalNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(decimalNumber), "Input must be a non - negative integer.");
+            }
+            if(decimalNumber==0)
+            {
+                return "0";
+            }
+            var binaryNumber = new int[32];
+            StringBuilder binaryNumberString = new StringBuilder();
+            int counter = 0;
+
+            while(decimalNumber>0)
+            {
+                binaryNumber[counter] = decimalNumber % 2;
+                decimalNumber /= 2;
+                counter += 1; //increment counter
+            }
+
+            // Construct the binary string in reverse order
+            for (var index = counter-1; index>=0; index--)
+            {
+                binaryNumberString.Append(binaryNumber[index]);
+            }
+            return binaryNumberString.ToString();
+        }
+
+        /// <summary>
+        /// Converts a binary number to its decimal equivalent.
+        /// </summary>
+        /// <param name="binary">A string representing a binary number.</param>
+        /// <returns>A string representing the decimal equivalent of the binary number.</returns>
+        public static int BinaryToDecimal(string binaryNumber)
+        {
+            return Convert.ToInt32(binaryNumber, 2);
         }
     }
 }
--- NumeralSystemConverterTest/BaseConverterTest.cs
+++ NumeralSystemConverterTest/BaseConverterTest.cs
@@ -8,11 +8,25 @@ namespace NumeralSystemConverterTest
         public void Setup()
         {
         }
-        [Test]
-        public void DecimalToBinaryTest()
+        [TestCase(int.MaxValue, "1111111111111111111111111111111")]
+        //[TestCase(7,"111")]
+        //[TestCase(10, "1010")]
+        //[TestCase(33, "100001")]
+        //[TestCase(34, "100010")]
+        [TestCase(255, "11111111")]
+        [TestCase(999999, "11110100001000111111")]
+        public void DecimalToBinaryTest(int decimalNumber,string expectedBinary)
         {
-            string result = BaseConverter.DecimalToBinary(33);
-            Assert.That(result,Is.EqualTo("100001"));
+            string result = BaseConverter.DecimalToBinary(decimalNumber);
+            Assert.That(result,Is.EqualTo(expectedBinary));
+        }
+
+        [TestCase("11111111",255)]
+        [TestCase("11110100001000111111", 999999)]
+        public void BinaryToDecimalTest(string binaryNumber,int expectedDecimalEquivalent)
+        {
+            var result = BaseConverter.BinaryToDecimal(binaryNumber);
+            Assert.That(result, Is.EqualTo(expectedDecimalEquivalent));
         }
     }
 }
